da2fc604831466ec5fd5ad800f79b96d11eaaea5
Johnny Broadway <johnny@johnnybroadway.com>
2011-11-21 14:31:32 -0600

    Added sqlite schema

---
+++ conf/install_sqlite.sql
@@ -0,0 +1,22 @@
+create table form (
+	id integer primary key,
+	title char(48) not null,
+	message text not null,
+	ts datetime not null,
+	fields text not null,
+	actions text not null,
+	response_title char(48) not null,
+	response_body text not null
+);
+
+create index form_title on form (title);
+create index form_ts on form (ts);
+
+create table results (
+	id integer primary key,
+	form_id int not null,
+	ts datetime not null,
+	results text not null
+);
+
+create index results_form on results (form_id, ts);
