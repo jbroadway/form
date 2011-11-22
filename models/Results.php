091dda20988d9a43df00a8bb37572077960d4d98
jbroadway <johnny@johnnybroadway.com>
2011-11-22 00:24:35 -0600

    Connected form\Results to form\Form and added JSON serialization to results

--- models/Results.php
+++ models/Results.php
@@ -2,11 +2,52 @@
 
 namespace form;
 
+/**
+ * This model stores individual form submissions in the `results`
+ * table.
+ */
 class Results extends \Model {
 	/**
 	 * Table name.
 	 */
 	public $table = 'results';
+
+	/**
+	 * Stores results data.
+	 */
+	private $_results = false;
+
+	/**
+	 * Create a reference to the form table.
+	 */
+	public $fields = array (
+		'form_id' => array ('ref' => 'form\Form')
+	);
+
+	/**
+	 * Dynamic getter that unserializes results.
+	 */
+	public function __get ($key) {
+		if ($key == 'results') {
+			if ($this->_results === false) {
+				$this->_results = json_decode ($this->data['results']);
+			}
+			return $this->_results;
+		}
+		return parent::__get ($key);
+	}
+
+	/**
+	 * Dynamic setter that serializes results.
+	 */
+	public function __set ($key, $val) {
+		if ($key == 'results') {
+			$this->_results = $val;
+			$this->data[$key] = json_encode ($val);
+			return;
+		}
+		return parent::__set ($key, $val);
+	}
 }
 
 ?>
