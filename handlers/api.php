3e870e26358817de5a4e30e42a911193abc43c0d
jbroadway <johnny@johnnybroadway.com>
2011-11-29 14:55:35 -0600

    API now uses new Restful class

--- handlers/api.php
+++ handlers/api.php
@@ -1,107 +1,11 @@
 <?php
 
 /**
- * Provides the JSON API for the form builder.
+ * Calls the JSON API for the form builder.
  */
 
-$page->layout = false;
-header ('Content-Type: application/json');
+$this->require_admin ();
 
-if (! User::require_admin ()) {
-	$this->redirect ('/admin');
-}
-
-$error = false;
-$msg = '';
-
-// Get and verify parameters
-list ($cmd, $id) = $this->params;
-
-if (! $cmd || empty ($cmd)) {
-	$error = i18n_get ('No command specified');
-} elseif (! $id) {
-	$error = i18n_get ('No form specified');
-} else {
-	switch ($cmd) {
-		case 'update';
-			/**
-			 * Update the core fields. Usage:
-			 *
-			 *     /form/api/update/form-id
-			 *
-			 * Expected POST fields:
-			 *
-			 *     title
-			 *     message
-			 *     response_title
-			 *     response_body
-			 */
-			$f = new form\Form ($id);
-			if ($f->error) {
-				$error = $f->error;
-				break;
-			}
-
-			$f->title = $_POST['title'];
-			$f->message = $_POST['message'];
-			$f->response_title = $_POST['response_title'];
-			$f->response_body = $_POST['response_body'];
-
-			$f->put ();
-			if ($f->error) {
-				$error = $f->error;
-				break;
-			}
-			$msg = i18n_get ('Form updated');
-			
-			break;
-
-		case 'actions';
-			/**
-			 * Update the core fields. Usage:
-			 *
-			 *     /form/api/actions/form-id
-			 *
-			 * Expected POST fields:
-			 *
-			 *     actions
-			 */
-			$f = new form\Form ($id);
-			if ($f->error) {
-				$error = $f->error;
-				break;
-			}
-
-			if (is_array ($_POST['actions'])) {
-				$f->actions = $_POST['actions'];
-			} else {
-				$f->actions = array ();
-			}
-
-			$f->put ();
-			if ($f->error) {
-				$error = $f->error;
-				break;
-			}
-			$msg = i18n_get ('Form updated');
-			
-			break;
-	
-		default:
-			$error = i18n_get ('Invalid command');
-	}
-}
-
-$res = new StdClass;
-if ($error) {
-	$res->success = false;
-	$res->error = $error;
-} else {
-	$res->success = true;
-	$res->msg = $msg;
-	$res->data = $out;
-}
-
-echo json_encode ($res);
+$this->restful (new form\API);
 
 ?>
---
+++ lib/API.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace form;
+
+/**
+ * Provides the JSON API for the form builder.
+ */
+class API extends \Restful {
+	/**
+	 * Update the core fields. Usage:
+	 *
+	 *     /form/api/update/form-id
+	 *
+	 * Expected POST fields:
+	 *
+	 *     title
+	 *     message
+	 *     response_title
+	 *     response_body
+	 */
+	public function post_update ($id) {
+		$f = new Form ($id);
+		if ($f->error) {
+			return $this->error (i18n_get ('Form not found'));
+		}
+		
+		$f->title = $_POST['title'];
+		$f->message = $_POST['message'];
+		$f->response_title = $_POST['response_title'];
+		$f->response_body = $_POST['response_body'];
+
+		$f->put ();
+		if ($f->error) {
+			return $this->error ('Failed to save changes');
+		}
+		
+		return i18n_get ('Form updated');
+	}
+
+	/**
+	 * Update the core fields. Usage:
+	 *
+	 *     /form/api/actions/form-id
+	 *
+	 * Expected POST fields:
+	 *
+	 *     actions
+	 */
+	public function post_actions ($id) {
+		$f = new Form ($id);
+		if ($f->error) {
+			return $this->error (i18n_get ('Form not found'));
+		}
+	
+		if (is_array ($_POST['actions'])) {
+			$f->actions = $_POST['actions'];
+		} else {
+			$f->actions = array ();
+		}
+	
+		$f->put ();
+		if ($f->error) {
+			return $this->error ('Failed to save changes');
+		}
+
+		return i18n_get ('Form updated');
+	}
+}
+
+?>
