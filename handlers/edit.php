e1eb37fd7f131b3bda08ecb494f8feaa1a813280
jbroadway <johnny@johnnybroadway.com>
2011-12-06 21:40:01 -0600

    Fields sidebar now scrolls with the page

--- handlers/edit.php
+++ handlers/edit.php
@@ -23,6 +23,7 @@ $page->add_script ('/apps/form/js/jquery.tmpl.js');
 $page->add_script ('/apps/form/js/knockout-1.2.1.js');
 $page->add_script ('/apps/form/js/formbuilder.js');
 $page->add_script ('/apps/form/css/formbuilder.css');
+$page->add_script ('/apps/form/js/waypoints.min.js');
 
 $page->add_script ('/apps/form/js/jquery.tools.min.js');
 $page->add_script ('/apps/form/css/rangeinput.css');
