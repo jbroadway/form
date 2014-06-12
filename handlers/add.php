86b1630c5090e862e761a7eaf1636c5cc7e137bc
BronyBorn <minecraftsolutionsinc@gmail.com>
2014-06-12 10:09:30 -0500

    Fixed incorrect versioning and redirect bugs for new forms.

--- handlers/add.php
+++ handlers/add.php
@@ -20,13 +20,19 @@ $f = new form\Form (array (
 	'response_body' => 'Your information has been saved.'
 ));
 $f->put ();
-\Versions::add ($f);
 
-if (! $f->error) {
-	$this->redirect ('/form/edit?id=' . $f->id);
+if ($f->error) {
+	$page->title = i18n_get ('An Error Occurred');
+	echo '<p>' . i18n_get ('Unable to create a new form.') . '</p>';
+	echo '<p class="notice">' . $f->error . '</p>';
+} else {
+	$f = form\Form::query()				// Since the first put for the form doesn't contain the id, it must be fetched from the DB.
+		->where('title','Untitled')		// Fetch similar title entries of which one we need.
+		->order('id desc')				 // Sort entry id from new to old.
+		->single();						// Grab the first entry (the one that was just put in).
+	\Versions::add ($f);
+	$this->redirect ('/form/edit?id='. $f->id);
 }
 
-$page->title = i18n_get ('An Error Occurred');
-echo '<p>' . i18n_get ('Unable to create a new form.') . '</p>';
 
 ?>
