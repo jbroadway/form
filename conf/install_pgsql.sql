cece2ac82189fd6aa63771dc6790d874a59ed5d9
BronyBorn <minecraftsolutionsinc@gmail.com>
2014-06-16 11:39:42 -0500

    Form add handler reverted to previous state Fixed data type bug

--- conf/install_pgsql.sql
+++ conf/install_pgsql.sql
@@ -1,11 +1,11 @@
 create table #prefix#form (
 	id serial not null primary key,
-	title character(48) not null,
+	title character varying(48) not null,
 	message text not null,
 	ts timestamp not null,
 	fields text not null,
 	actions text not null,
-	response_title character(48) not null,
+	response_title character varying(48) not null,
 	response_body text not null
 );
 create index title on #prefix#form (title);
@@ -16,6 +16,6 @@ create table #prefix#form_results (
 	form_id integer not null,
 	ts timestamp not null,
 	ip character(15) not null,
-	results text not null
+	results text not null 
 );
 create index form_id on #prefix#form_results (form_id, ts);
