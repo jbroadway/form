57f0f6a8031bc4826fa0457ffb05885393f40def
lux <john@flipsidexr.com>
2022-04-06 18:27:03 -0500

    Ensure read table has primary key

--- conf/install_mysql.sql
+++ conf/install_mysql.sql
@@ -24,6 +24,7 @@ create table #prefix#form_read (
 	form_id int not null,
 	results_id int not null,
 	user_id int not null,
+	primary key (form_id, results_id, user_id),
 	index (form_id, user_id),
 	index (results_id, user_id)
 );
